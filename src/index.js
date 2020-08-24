4e37bb610343384b7c42b693c92a42104e3302a7
sashida <sashida@primagest.co.jp>
2020-08-25 01:47:08 +0900

    p.105

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import "bootstrap/dist/css/bootstrap.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
