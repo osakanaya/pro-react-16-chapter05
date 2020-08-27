7f520119b9ffa90f7bd0ba820632512ebb19850b
sashida <sashida@primagest.co.jp>
2020-08-28 01:59:17 +0900

    p.161

--- src/data/RestDataSource.js
+++ src/data/RestDataSource.js
@@ -2,7 +2,13 @@ import Axios from "axios";
 import { RestUrls } from "./Urls";
 
 export class RestDataSource {
-    SendRequest = (method, url, params) => Axios.request({ method, url, params });
+    constructor(err_handler) {
+        this.error_handler = err_handler || (() => {});
+    }
+
+    SendRequest = (method, url, params, data) => Axios.request({ method, url, params, data });
 
     GetData = async(dataType, params) => this.SendRequest("get", RestUrls[dataType], params);
+
+    StoreData = (dataType, data) => this.SendRequest("post", RestUrls[dataType], {}, data);
 }
