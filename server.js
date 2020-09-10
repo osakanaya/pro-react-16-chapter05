db9b1ca11448b2af81905a2fc09f437b24d356dc
sashida <sashida@primagest.co.jp>
2020-09-11 04:08:31 +0900

    p.210

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const mutationResolvers = require("./serverMutationsResolver");
 
 const auth = require("./authMiddleware");
 
+const history = require("connect-history-api-fallback");
+
 const fileName = process.argv[2] || "./data.js";
 const port = process.argv[3] || 3500;
 
@@ -38,6 +40,8 @@ const createServer = () => {
 
 createServer();
 
+app.use(history());
+app.use("/", express.static("./build"));
 app.use(cors());
 app.use(jsonServer.bodyParser);
 app.use(auth);
